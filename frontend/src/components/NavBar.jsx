bb0dfd4b7cbd5c9c375a905523d15960b849855a
jeremie-cell <jeremiebalondon8@gmail.com>
2026-04-05 11:08:43 +0800

    navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          Jeremie Balondon
+          Portfolio
         </a>
         <nav className="desktop">
           <ul>
